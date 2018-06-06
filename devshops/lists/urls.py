915a52b31e2952339a10c206db5173644e8d41e7
Takumi Nishida <tnishida1@mail.csuchico.edu>
2018-06-06 10:50:42 -0700

    huge ass commit with bootstrap, initial pages, set up

--- devshops/lists/urls.py
+++ devshops/lists/urls.py
@@ -1,7 +1,14 @@
-from django.urls import path
+from django.conf.urls import url, include
+from django.contrib import admin
+from django.conf import settings
 
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index'),
+    url(r'^admin/', admin.site.urls),
+    url(r'^$', views.home, name='home'),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- devshops/lists/views.py
+++ devshops/lists/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
-from django.http import HttpResponse
 
-def index(request):
-    return HttpResponse("Hello, world. You're at the list index.")
+def home(request):
+    context = locals()
+    template = 'home.html'
+    return render(request, template, context)
